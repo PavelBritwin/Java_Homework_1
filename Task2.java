43754fa9505fd1a90c3cac734490232f07de4c86
Pavel <anohinpa@gmail.com>
2023-01-21 18:26:16 +0300

    Task2

---
+++ Task2.java
@@ -0,0 +1,24 @@
+package Java_homework_1;
+
+public class Task2 {
+    public static void main(String[] args) {
+        String seqSimpleNumbers = "2";
+        for (int i = 3; i <= 1000; i++)
+        {
+            int k = 2;
+            while (k < i)
+            {
+                if (i % k == 0)
+                {
+                    break;
+                }
+                if (k == i - 1)
+                {
+                    seqSimpleNumbers += " "+Integer.toString(i);
+                }
+                k++;
+            }
+        }
+        System.out.println(seqSimpleNumbers);
+    }
+}
